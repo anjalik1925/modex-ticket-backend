5d6d0890644ab7541a1988938534c83cf4bdbca4
anjalik1925 <anjalikumari19025@gmail.com>
2025-12-11 23:23:11 +0530

    Fix db connection and add dockerignore

--- src/db.js
+++ src/db.js
@@ -1,23 +1,11 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-async function withTransaction(fn) {
-  const client = await pool.connect();
-  try {
-    await client.query("BEGIN");
-    const result = await fn(client);
-    await client.query("COMMIT");
-    return result;
-  } catch (err) {
-    await client.query("ROLLBACK");
-    throw err;
-  } finally {
-    client.release();
-  }
-}
-
-module.exports = { pool, withTransaction };
+export default pool;
